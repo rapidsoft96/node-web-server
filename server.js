8bc604cc8a330e98eb3d14c13ca663a7ac740e40
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-04-23 15:07:46 +0200

    Making new page Projects

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about',(req,res)=>{
 
   });
 });
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle: 'Projects',
+    message: 'my projects'
+  });
+});
 
 app.get('/bad',(req,res)=>{
   res.send({
